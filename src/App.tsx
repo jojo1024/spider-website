b169e71a34c22e6df2e50911ff622fc891ea4f34
Ndjabo <joelithondjabo16@gmail.com>
2023-05-09 16:49:25 +0000

    bb

--- src/App.tsx
+++ src/App.tsx
@@ -36,146 +36,146 @@ import { setAllContact } from './store/contactSlice';
 
 
 function App() {
-  const dispatch = useDispatch();
-  const { serverUrl } = useSelector((state: IReduxState) => state.application);
+  // const dispatch = useDispatch();
+  // const { serverUrl } = useSelector((state: IReduxState) => state.application);
 
-  const [cookies] = useCookies(["serverUrl"]);
-
-
-
-  /**
-  * au demarrage de l'appli, on récupère l'adresse
-  * du serveur, via les cookies si on est en mode prod.
-  * En mode dev, l'adresse est en dure dans le store
-  * */
-  useEffect(() => {
-    if (!isDev()) dispatch(setServerUrl(cookies.serverUrl));
-  }, []);
-
-  useEffect(() => {
-    if (serverUrl.length > 0) {
-      socketIO.initialize();
-    }
-  }, [serverUrl]);
-
-  React.useEffect(() => {
-    const wow = new WOW.WOW({
-      offset: 100,
-      mobile: false,
-      live: true,
-    });
-    wow.init();
-  }, []);
+  // const [cookies] = useCookies(["serverUrl"]);
 
 
 
-  // Fonctions
-  const recupActualte = async () => {
-    try {
-      const res: any = await apiClient.get(
-        SERVER_BASE_URL,
-        "/recupactualite"
-      );
-      if (res.status === 1) {
-        dispatch(setAllActualite(res.data))
-      }
-    } catch (error) {
+  // /**
+  // * au demarrage de l'appli, on récupère l'adresse
+  // * du serveur, via les cookies si on est en mode prod.
+  // * En mode dev, l'adresse est en dure dans le store
+  // * */
+  // useEffect(() => {
+  //   if (!isDev()) dispatch(setServerUrl(cookies.serverUrl));
+  // }, []);
 
-    } finally {
-      dispatch(setLoadingActualite(false))
-    }
-  };
+  // useEffect(() => {
+  //   if (serverUrl.length > 0) {
+  //     socketIO.initialize();
+  //   }
+  // }, [serverUrl]);
 
-
-  const recupAgent = async () => {
-    try {
-      const res: any = await apiClient.get(
-        SERVER_BASE_URL,
-        "/recupagent"
-      );
-      if (res.status === 1) {
-        dispatch(setAllAgent(res.data))
-      }
-    } catch (error) {
-
-    } finally {
-      dispatch(setLoadingAgent(false))
-    }
-  };
-
-  const recupImageParDossier = async () => {
-    try {
-      const res: any = await apiClient.get(
-        SERVER_BASE_URL,
-        "/recupimagepardossier"
-      );
-      if (res.status === 1) {
-        dispatch(setAllDossier(res.data))
-      }
-    } catch (error) {
-    } finally {
-      dispatch(setLoadingGalerie(false))
-    }
-  };
-
-  const recupSpiderAllVersions = async () => {
-    try {
-      const res: any = await apiClient.get(
-        SERVER_BASE_URL,
-        "/recupspiderallversions"
-      );
-      if (res.status === 1) {
-        let newData = []
-        newData = res.data.map((item: ISpiderVersion) => { return { ...item, versionDate: new Date(item.versionDate).toLocaleDateString() } })
-        dispatch(setAllSpiderVersions(newData))
-      }
-    } catch (error) {
-    } finally {
-      dispatch(setLoadingSpiderVersion(false))
-    }
-  };
-
-  // Obtenir toutes les applications de Spider Technologies
-  const recupApplication = async () => {
-    try {
-      const res: any = await apiClient.get(
-        SERVER_BASE_URL,
-        "/recupapplication"
-      );
-      if (res.status === 1) {
-        dispatch(setAllSpiderApp(res.data))
-      }
-    } catch (error) {
-    } finally {
-      dispatch(setLoadindSpiderApp(false))
-    }
-  };
+  // React.useEffect(() => {
+  //   const wow = new WOW.WOW({
+  //     offset: 100,
+  //     mobile: false,
+  //     live: true,
+  //   });
+  //   wow.init();
+  // }, []);
 
 
 
-  const recupContact = async () => {
-    try {
-      const res: any = await apiClient.get(
-        SERVER_BASE_URL,
-        "/recupcontact"
-      );
-      if (res.status === 1) {
-        dispatch(setAllContact(res.data))
-      }
-    } catch (error) {
-    } finally {
-    }
-  };
+  // // Fonctions
+  // const recupActualte = async () => {
+  //   try {
+  //     const res: any = await apiClient.get(
+  //       SERVER_BASE_URL,
+  //       "/recupactualite"
+  //     );
+  //     if (res.status === 1) {
+  //       dispatch(setAllActualite(res.data))
+  //     }
+  //   } catch (error) {
+
+  //   } finally {
+  //     dispatch(setLoadingActualite(false))
+  //   }
+  // };
 
 
-  useEffect(() => {
-    recupActualte();
-    recupSpiderAllVersions();
-    recupAgent();
-    recupImageParDossier();
-    recupApplication();
-    recupContact();
-  }, [])
+  // const recupAgent = async () => {
+  //   try {
+  //     const res: any = await apiClient.get(
+  //       SERVER_BASE_URL,
+  //       "/recupagent"
+  //     );
+  //     if (res.status === 1) {
+  //       dispatch(setAllAgent(res.data))
+  //     }
+  //   } catch (error) {
+
+  //   } finally {
+  //     dispatch(setLoadingAgent(false))
+  //   }
+  // };
+
+  // const recupImageParDossier = async () => {
+  //   try {
+  //     const res: any = await apiClient.get(
+  //       SERVER_BASE_URL,
+  //       "/recupimagepardossier"
+  //     );
+  //     if (res.status === 1) {
+  //       dispatch(setAllDossier(res.data))
+  //     }
+  //   } catch (error) {
+  //   } finally {
+  //     dispatch(setLoadingGalerie(false))
+  //   }
+  // };
+
+  // const recupSpiderAllVersions = async () => {
+  //   try {
+  //     const res: any = await apiClient.get(
+  //       SERVER_BASE_URL,
+  //       "/recupspiderallversions"
+  //     );
+  //     if (res.status === 1) {
+  //       let newData = []
+  //       newData = res.data.map((item: ISpiderVersion) => { return { ...item, versionDate: new Date(item.versionDate).toLocaleDateString() } })
+  //       dispatch(setAllSpiderVersions(newData))
+  //     }
+  //   } catch (error) {
+  //   } finally {
+  //     dispatch(setLoadingSpiderVersion(false))
+  //   }
+  // };
+
+  // // Obtenir toutes les applications de Spider Technologies
+  // const recupApplication = async () => {
+  //   try {
+  //     const res: any = await apiClient.get(
+  //       SERVER_BASE_URL,
+  //       "/recupapplication"
+  //     );
+  //     if (res.status === 1) {
+  //       dispatch(setAllSpiderApp(res.data))
+  //     }
+  //   } catch (error) {
+  //   } finally {
+  //     dispatch(setLoadindSpiderApp(false))
+  //   }
+  // };
+
+
+
+  // const recupContact = async () => {
+  //   try {
+  //     const res: any = await apiClient.get(
+  //       SERVER_BASE_URL,
+  //       "/recupcontact"
+  //     );
+  //     if (res.status === 1) {
+  //       dispatch(setAllContact(res.data))
+  //     }
+  //   } catch (error) {
+  //   } finally {
+  //   }
+  // };
+
+
+  // useEffect(() => {
+  //   recupActualte();
+  //   recupSpiderAllVersions();
+  //   recupAgent();
+  //   recupImageParDossier();
+  //   recupApplication();
+  //   recupContact();
+  // }, [])
 
 
   return (
@@ -183,11 +183,11 @@ function App() {
 
       <div className="container-xxl bg-white p-0">
 
-        <Header />
+        {/* <Header /> */}
         <div >
           <Routes>
-            <Route path="spider-website/" element={<Home />} />
-            <Route path="spider-website/about" element={<About />} />
+            <Route path="spider-website/" element={<About />} />
+            {/* <Route path="spider-website/about" element={<About />} />
             <Route path="spider-website/produits" element={<Produits />} />
             <Route path="spider-website/equipe" element={<Agent />} />
             <Route path="spider-website/blog" element={<Blog />} />
@@ -198,7 +198,7 @@ function App() {
             <Route path="spider-website/spider-galerie-detail" element={<GalerieDetail />} />
             <Route path="spider-website/actualite" element={<Actualite />} />
             <Route path="spider-website/mise-a-jour-anterieur" element={<SpiderMiseAJour />} />
-            <Route path="spider-website/*" element={<ErrorPage />} />
+            <Route path="spider-website/*" element={<ErrorPage />} /> */}
           </Routes>
         </div>
         <Footer />
